a53173a9c6cdc01bb3d070346fcd24e7b1672b0e
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2014-12-30 16:00:10 +0800

    Initial upload

---
+++ server.js
@@ -0,0 +1,71 @@
+//Copyright 2013-2014 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//Licensed under the Apache License, Version 2.0 (the "License"). 
+//You may not use this file except in compliance with the License. 
+//A copy of the License is located at
+//
+//    http://aws.amazon.com/apache2.0/
+//
+//or in the "license" file accompanying this file. This file is distributed 
+//on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+//either express or implied. See the License for the specific language 
+//governing permissions and limitations under the License.
+
+//Get modules.
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+var fs = require('fs');
+var AWS = require('aws-sdk');
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+app.locals.theme = process.env.THEME; //Make the THEME environment variable available to the app. 
+
+//Read config values from a JSON file.
+var config = fs.readFileSync('./app_config.json', 'utf8');
+config = JSON.parse(config);
+
+//Create DynamoDB client and pass in region.
+var db = new AWS.DynamoDB({region: config.AWS_REGION});
+
+//GET home page.
+app.get('/', routes.index);
+
+//POST signup form.
+app.post('/signup', function(req, res) {
+	var platformField = req.body.platform,
+	appIdField = req.body.appId;
+  res.send(200);
+  resolve(platformField, appIdField;
+});
+
+//Add signup form data to database.
+var resolve = function (platform, appId) {
+  var formData = {
+    TableName: config.APP_TABLE,
+    Item: {
+      app_id: {'S': appId}, 
+		platform: {'S': platform}
+    }
+  };
+  db.putItem(formData, function(err, data) {
+    if (err) {
+      console.log('Error adding item to database: ', err);
+    } else {
+      console.log('Form data added to database.');
+    }
+  });
+};
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
