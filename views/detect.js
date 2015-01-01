3fb5fbf4911a4ebb73d8c9f1b440749b530d0454
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2015-01-01 14:38:10 +0800

    browser detect++

--- views/detect.js
+++ views/detect.js
@@ -1,6 +1,16 @@
 
 
 function detect(data){
-	var os = platform.os;	
-	document.write(os)
+	document.write(data);
+	var os = platform.os;
+	
+	if(os.startsWith('iOS')){
+		document.write(data.Item.itunes.S)
+	}
+	else if(os.startsWith('Android')){
+		document.write(data.Item.google_play.S)
+	}
+	else{
+		document.write(os)
+	}
 }
