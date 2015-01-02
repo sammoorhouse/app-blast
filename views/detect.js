ed56f0e663ab981d0b77759116bc6978fd78ddec
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2015-01-02 17:39:55 +0800

    check existence of relevant data; redirect to homepage (?)

--- views/detect.js
+++ views/detect.js
@@ -1,13 +1,19 @@
 
 function detect(data){
+	var unded = "undefined";
 	var os = platform.os.family;
-	if(os.lastIndexOf('iOS', 0) === 0){
-		window.location.replace('https://itunes.apple.com/app/' + data.Item.itunes.S)
+    /* Common values include:
+    * "Windows", "Windows Server 2008 R2 / 7", "Windows Server 2008 / Vista",
+    * "Windows XP", "OS X", "Ubuntu", "Debian", "Fedora", "Red Hat", "SuSE",
+    * "Android", "iOS" and "Windows Phone"
+	*/
+	if((os.lastIndexOf('iOS', 0) === 0) && (typeof(data.Item.itunes) != undef){
+		window.location.replace('https://itunes.apple.com/app/' + data.Item.itunes.S);
 	}
-	else if(os.lastIndexOf('Android', 0) === 0){
-		window.location.replace('https://play.google.com/store/apps/details?id=' + data.Item.google_play.S)
+	else if((os.lastIndexOf('Android', 0) === 0) && (typeof(data.Item.google_play) != undef)){
+		window.location.replace('https://play.google.com/store/apps/details?id=' + data.Item.google_play.S);
 	}
 	else{
-		document.write(os)
+		window.location.replace('/');
 	}
 }
