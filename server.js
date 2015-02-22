0e05dd5dce6cfaf48ebb429d3d4c408072168ba6
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2015-02-22 15:40:20 +0800

    remove favicon ref?

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-//Copyright 2013-2014 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-//Licensed under the Apache License, Version 2.0 (the "License"). 
-//You may not use this file except in compliance with the License. 
-//A copy of the License is located at
-//
-//    http://aws.amazon.com/apache2.0/
-//
-//or in the "license" file accompanying this file. This file is distributed 
-//on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
-//either express or implied. See the License for the specific language 
-//governing permissions and limitations under the License.
-
 //Get modules.
 var express = require('express');
 var http = require('http');
@@ -23,12 +11,11 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.static(__dirname + '/public'));
-app.use(app.router);
+//app.use(app.router);
 
 
 //Read config values from a JSON file.
@@ -69,14 +56,10 @@ var resolve = function(appId, cb) {
 			console.log('Error getting data from database: ', err);
 			cb(err)
 		} else {
-			console.log('Got data:', data);
+			console.log('Got data: ', data);
 			cb(data)
 		}
 	});
 };
 
 //app.get('/add')
-
-http.createServer(app).listen(app.get('port'), function() {
-	console.log('Express server listening on port ' + app.get('port'));
-});
