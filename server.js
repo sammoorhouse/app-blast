a532e3cf66bb4c69ed2dd67433b7a7e707394bbe
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2017-02-03 16:48:33 +0700

    restart

--- server.js
+++ server.js
@@ -2,61 +2,18 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
-var fs = require('fs');
-var AWS = require('aws-sdk');
 //var jade = require('jade')
 
 var app = express();
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, '/views'));
-app.set('view engine', 'jade');
 
-require('./routes/index')
+app.get('/res/:id', (req, res)=>{
+//get datas
 
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.static(path.join(__dirname, '/public')));
+//redirect
 
-//Read config values from a JSON file.
-var config = fs.readFileSync('./app_config.json', 'utf8');
-config = JSON.parse(config);
-
-//Create DynamoDB client and pass in region.
-var db = new AWS.DynamoDB({
-	region: config.AWS_REGION
-});
-
-//GET resolve
-app.get('/resolve', function(req, res) {
-	var appIdField = req.query.appId;
-	console.log('resolving', appIdField);
-	resolve(appIdField, function(data) {
-		console.log('got data ', data);
-		res.render('template', {data: data})
-	});
-});
-
-var resolve = function(appId, cb) {
-	var params = {
-		TableName: 'apps',
-		Key: {
-			"app_id": {
-				"S": appId
-			},
-		}
-	}
-	db.getItem(params, function(err, data) {
-		console.log('sent', params)
-		if (err) {
-			console.log('Error getting data from database: ', err);
-			cb(err)
-		} else {
-			console.log('Got data: ', data);
-			cb(data)
-		}
-	});
-};
+window.location.replace('https://play.google.com/store/apps/details?id=12345);
+})
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
